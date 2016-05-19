7d4818f069b18a39844985b1e35870feaea65455
Popcore <smallzed@gmail.com>
2016-05-19 22:31:37 +0100

    displaying loading wheel while waiting for API response

--- js/functions.js
+++ js/functions.js
@@ -7,8 +7,7 @@ jQuery(document).ready(function($) {
 	var airQualityResult = null;
 	var weatherResult = null;
 	var lat, lon;
-	var content = $('#content');
-	var report = ""; // the final output that will be displayed
+	var errors; 
 
 	// Find user geolocation
 	function getLocation() {
@@ -16,18 +15,18 @@ jQuery(document).ready(function($) {
 		$('button').remove();
 
 	    if (navigator.geolocation) {
-	    	report += "Getting geolocation..."
-	    	console.log("Getting geolocation...");
+	    	var logs = "Getting geolocation...";
+	    	
 	    	// call getData once received geolocation
 	        navigator.geolocation.getCurrentPosition(getData, showError);
 
 
 	    } else { 
-	    	report += "Geolocation is not supported by this browser..."
+	    	errors += "Geolocation is not supported by this browser..."
 	    	console.log("Geolocation is not supported by this browser.");
 	    }
 
-	    content.html(report);
+	    $( "#content" ).append( '<div class="logs"><p>' + logs + '</p></div>' );
 	}
 
 
@@ -35,19 +34,19 @@ jQuery(document).ready(function($) {
 	function showError(error) {
 	    switch(error.code) {
 	        case error.PERMISSION_DENIED:
-	        	content.html("User denied the request for Geolocation.");
+	        	$('.errors').html("User denied the request for Geolocation.");
 	            console.log("User denied the request for Geolocation.");
 	            break;
 	        case error.POSITION_UNAVAILABLE:
-	        	content.html("Location information is unavailable.");
+	        	$('.errors').html("Location information is unavailable.");
 	            console.log("Location information is unavailable.");
 	            break;
 	        case error.TIMEOUT:
-	        	content.html("The request to get user location timed out.");
+	        	$('.errors').html("The request to get user location timed out.");
 	            console.log("The request to get user location timed out.");
 	            break;
 	        case error.UNKNOWN_ERROR:
-	        	content.html("An unknown error occurred.");
+	        	$('.errors').html("An unknown error occurred.");
 	            console.log("An unknown error occurred.");
 	            break;
 	    }
@@ -61,50 +60,45 @@ jQuery(document).ready(function($) {
 
 		lat = position.coords.latitude;
 	    lon = position.coords.longitude;
-
-	    report += "done<br><br>"
-	    content.html(report);
-
 	    
-		report += "Requesting for bike <br>"
+		var logs = "Requesting for bike<br>"
 		thingful.get('q=bike&lat='+lat+'long='+lon+'radius=500&sort=distance&limit=5', function(data){
 			bikeResult = data;
 			allDataReceived();
 		});
 
-		report += "Requesting for air quality <br>"
+		logs += "Requesting for air quality<br>"
 		thingful.get('q=airquality&lat='+lat+'long='+lon+'radius=500&sort=distance&limit=5', function(data){
 			airQualityResult = data;
 			allDataReceived();
 		});
 
-		report += "Requesting for weather <br><br>"
+		logs += "Requesting for weather<br><br>"
 		thingful.get('q=weather&lat='+lat+'long='+lon+'radius=500&sort=distance&limit=5', function(data){
 			weatherResult = data;
 			allDataReceived();
 		});
 
-		report += "Waiting for responses...<br>";
-		content.html(report);
+		logs += "Waiting for responses...";
+		$( "#content" ).find('.logs').append('<p>' + logs + '</p>');
+		$( "#content" ).find('.logs').append('<img id="loading-wheel" src="/images/loading-wheel.svg" />');
+
+		// show loading wheel
+		$('#loading-wheel ').fadeIn('slow');
 	}
 
 	// Ensure all the necessary data is available before processing the data
 	function allDataReceived(){
+
 		if (bikeResult && airQualityResult && weatherResult) {
-			report += "All responses received <br><br>"
-			content.html(report);
 
 			console.log("All responses received");
 			console.log(bikeResult);
 			console.log(airQualityResult);
 			console.log(weatherResult);
-
-			content.html(report);
+			
 			processValue();
-		} else {
-			return;
 		}
-
 	}
 
 	// Find the values that we want to process.
@@ -113,6 +107,11 @@ jQuery(document).ready(function($) {
 	// to availability (which is what we are interested in). 
 	// It is recommended to always check values before using them.
 	function processValue(){
+		$('#loading-wheel ').fadeOut('fast');
+		
+		var response = '';
+		var summary = '<h3>SUMMARY</h3>';
+
 		var bikeID = bikeResult.data[0].attributes.channels[0].id;
 		var bikeAvailable = bikeResult.data[0].attributes.channels[0].value;
 
@@ -127,36 +126,41 @@ jQuery(document).ready(function($) {
 		var bikeToday = true; // this is the answer, with default value to true
 
 		if (bikeAvailable == 0){ 
-			report += "There is no bike for you<br>";
+			summary += "<p>There is no bike for you</p>";
 			bikeToday = false; // return false if conditions are not met
 		} else {
-			report += "There are " + bikeAvailable + " bikes for you<br>";
+			summary += "<p>There are " + bikeAvailable + " available bikes for you<p/>";
 		}
 
 		// our arbitary threshold for air quality index
 		if (airQuality < 50) { 
 			bikeToday = false;
-			report += "Air quality is bad<br>";
+			summary += "<p>Air quality is bad</p>";
 		} else {
-			report += "Air quality is good: "+airQuality+"<br>";
+			summary += "<p>Air quality is good: "+airQuality+"</p>";
 		}
 
 		// our arbitary threshold for temperature
 		if (temperature < 10) { 
 			bikeToday = false;
-			report += "But it's too cold: "+temperature+"<br><br>";
+			summary += "<p>But it's too cold: "+temperature+"</p>";
 		} else {
-			report += "temperature is nice:" + temperature + "<br><br>";
+			summary += "<p>Temperature is nice: " + temperature + "</p>";
 		}
+
+		// display summary
+		$( "#content" ).append( '<div id="summary">' + summary + '</div>' );
+		
 
 		// finally display the result
 		if (bikeToday) { 
-			report += "It's a good day to cycle!!!";
+			response += "<div id='reponse-ok'><h2>It's a good day to cycle!</h2></div>";
 		} else {
-			report += "It's not a good day to cycle";
+			response += "<div id='reponse-bad'><h2>It's not a good day to cycle</div>";
 		}
 
-		content.html(report);
+		// display response
+		$( "#content" ).append( '<div id="response">' + response + '</div>' );
 	}
 
 	$('button').on('click', getLocation);
